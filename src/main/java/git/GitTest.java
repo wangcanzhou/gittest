a7f25d09717cd1063e942f5c8b49b76391a6bdda
cancan <18300621591@163.com>
2021-08-09 10:53:30 +0800

    combine later four commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello githot-fix");
+        System.out.println("hello git combinelater4");
     }
 }
